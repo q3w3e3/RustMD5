297d4c5ad5026465f32eae9321622c6973c43e30
q3w3e3 <charlotte.woodrow@q3w3e3.dev>
2021-03-19 18:16:18 -0400

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use md5::{Md5, Digest};
+use md5::{Digest, Md5};
+use num_cpus;
 use rand::Rng;
 use std::cmp;
-use threadpool::ThreadPool;
-use std::process;
-use num_cpus;
 use std::convert::TryInto;
+use std::process;
+use threadpool::ThreadPool;
 
 fn brute() {
     let mut hasher = Md5::new();
@@ -12,7 +12,7 @@ fn brute() {
 
     let seed = rng.gen::<u128>();
 
-    println!("seed: {:032x}",seed);
+    println!("seed: {:032x}", seed);
     hasher.update(format!("{:032x}", seed));
 
     let mut max: [u8; 16] = [127; 16];
@@ -33,32 +33,32 @@ fn brute() {
         let previous_as_array: [u8; 16] = previous.as_slice().try_into().expect("incorrect length");
 
         // check if fixed point
-        if result == previous{
+        if result == previous {
             println!("{:032x}", result);
             process::exit(0);
         }
-        // check if new max 
-        if result_as_array > max{
+        // check if new max
+        if result_as_array > max {
             println!("{}", hex::encode(max));
         }
-        max = cmp::max(max,result_as_array);
+        max = cmp::max(max, result_as_array);
 
         // check if new min hash
-        if result_as_array < min{
+        if result_as_array < min {
             println!("{}", hex::encode(min));
         }
-        min = cmp::min(min,result_as_array);
+        min = cmp::min(min, result_as_array);
 
         // check prefix}
-        let pref_len: u8 = check_prefix(result_as_array,previous_as_array, 0);
-        if  pref_len >= match_threshold {
+        let pref_len: u8 = check_prefix(result_as_array, previous_as_array, 0);
+        if pref_len >= match_threshold {
             println!("prefix x:      {:032x}", previous);
             println!("prefix md5(x): {:032x}", result);
         }
 
         // check suffix
-        let suff_len: u8 = 16 - check_suffix(result_as_array,previous_as_array, 15);
-        if  suff_len >= match_threshold {
+        let suff_len: u8 = 16 - check_suffix(result_as_array, previous_as_array, 15);
+        if suff_len >= match_threshold {
             println!("suffix x:      {:032x}", previous);
             println!("suffix md5(x): {:032x}", result);
         }
@@ -69,18 +69,17 @@ fn brute() {
 }
 
 fn check_prefix(first: [u8; 16], second: [u8; 16], mut i: u8) -> u8 {
-
-    if first[i as usize] == second[i as usize]{
+    if first[i as usize] == second[i as usize] {
         i += 1;
-        return check_prefix(first,second,i);
+        return check_prefix(first, second, i);
     }
     return i;
 }
 
 fn check_suffix(first: [u8; 16], second: [u8; 16], mut i: u8) -> u8 {
-    if first[i as usize] == second[i as usize]{
+    if first[i as usize] == second[i as usize] {
         i -= 1;
-        return check_suffix(first,second,i);
+        return check_suffix(first, second, i);
     }
     return i;
 }
@@ -91,7 +90,7 @@ fn main() {
     let pool = ThreadPool::new(n_workers);
     // let n_jobs = 1;
 
-    for _ in 0..n_jobs{
+    for _ in 0..n_jobs {
         pool.execute(|| brute());
     }
     pool.join();
