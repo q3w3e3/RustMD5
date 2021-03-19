022929bd783197b7416591ac853801a091da3847
q3w3e3 <charlotte.woodrow@q3w3e3.dev>
2021-03-19 17:32:22 -0400

    removed Bad uses of strings

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::cmp;
 use threadpool::ThreadPool;
 use std::process;
 use num_cpus;
+use std::convert::TryInto;
 
 fn brute() {
     let mut hasher = Md5::new();
@@ -14,9 +15,9 @@ fn brute() {
     println!("seed: {:032x}",seed);
     hasher.update(format!("{:032x}", seed));
 
-    let mut max: String = "fffffffff00000000000000000000000".to_string();
-    let mut min: String = "000000000fffffffffffffffffffffff".to_string();
-    let mut match_threshold: u16 = 10;
+    let mut max: [u8; 16] = [127; 16];
+    let mut min: [u8; 16] = [1; 16];
+    let mut match_threshold: u8 = 10;
 
     let mut result = hasher.finalize();
 
@@ -28,71 +29,67 @@ fn brute() {
         hasher.update(format!("{:032x}", result));
         result = hasher.finalize();
 
+        let result_as_array: [u8; 16] = result.as_slice().try_into().expect("incorrect length");
+        let previous_as_array: [u8; 16] = previous.as_slice().try_into().expect("incorrect length");
+
         // check if fixed point
         if result == previous{
             println!("{:032x}", result);
             process::exit(0);
         }
-
-        // check if new max hash
-        if format!("{:032x}", result) > max {
-            println!("max x:      {:032x}", previous);
-            println!("max md5(x): {:032x}", result);
+        // check if new max 
+        if result_as_array > max{
+            println!("{}", hex::encode(max));
         }
-        max = cmp::max(max,format!("{:032x}", result));
+        max = cmp::max(max,result_as_array);
 
         // check if new min hash
-        if format!("{:032x}", result) < min {
-            println!("min x:      {:032x}", previous);
-            println!("min md5(x): {:032x}", result);
+        if result_as_array < min{
+            println!("{}", hex::encode(min));
         }
-        min = cmp::min(min,format!("{:032x}", result));
+        min = cmp::min(min,result_as_array);
 
-        // check prefix
-        let pref_len: u16 = check_prefix(format!("{:032x}", previous),format!("{:032x}", result),0,false);
+        // check prefix}
+        let pref_len: u8 = check_prefix(result_as_array,previous_as_array, 0);
         if  pref_len >= match_threshold {
             println!("prefix x:      {:032x}", previous);
             println!("prefix md5(x): {:032x}", result);
         }
 
         // check suffix
-        let suff_len: u16 = check_prefix(format!("{:032x}", previous),format!("{:032x}", result),0,true);
-        if suff_len >= match_threshold {
+        let suff_len: u8 = 16 - check_suffix(result_as_array,previous_as_array, 15);
+        if  suff_len >= match_threshold {
             println!("suffix x:      {:032x}", previous);
             println!("suffix md5(x): {:032x}", result);
         }
 
+        // set new threshold
         match_threshold = cmp::max(cmp::max(pref_len, suff_len), match_threshold);
     }
 }
 
-fn check_prefix(mut first: String, mut second: String, mut count: u16,reversed: bool) -> u16 {
-    if reversed{
-        first = first.chars().rev().collect();
-        second = second.chars().rev().collect();
+fn check_prefix(first: [u8; 16], second: [u8; 16], mut i: u8) -> u8 {
+
+    if first[i as usize] == second[i as usize]{
+        i += 1;
+        return check_prefix(first,second,i);
     }
-    if first.chars().count() == 0{
-        return count;
-    }
-    else if first.chars().nth(0).unwrap() == second.chars().nth(0).unwrap(){
-        count += 1;
-        return check_prefix(without_first(first.as_str()).to_string(), without_first(second.as_str()).to_string(),count,false);
-    }
-    return count;
+    return i;
 }
 
-fn without_first(string: &str) -> &str {
-    string
-        .char_indices()
-        .next()
-        .and_then(|(i, _)| string.get(i + 1..))
-        .unwrap_or("")
+fn check_suffix(first: [u8; 16], second: [u8; 16], mut i: u8) -> u8 {
+    if first[i as usize] == second[i as usize]{
+        i -= 1;
+        return check_suffix(first,second,i);
+    }
+    return i;
 }
 
 fn main() {
     let n_workers = num_cpus::get();
     let n_jobs = num_cpus::get();
     let pool = ThreadPool::new(n_workers);
+    // let n_jobs = 1;
 
     for _ in 0..n_jobs{
         pool.execute(|| brute());
